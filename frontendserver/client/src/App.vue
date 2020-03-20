8fc9e049db9bdc8f5616c5b055d78cc5aa38c572
CStarn <Christoph.Starnecker@gmail.com>
2020-03-20 23:45:59 +0100

    Use nicer way for defining the mongodb save path

--- frontendserver/client/src/App.vue
+++ frontendserver/client/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>HACKATUM</span>
+        <span>#WirVsVirus</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
