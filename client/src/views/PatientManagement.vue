452685bb5d6eb4a1b9a9158ab4a2e287561adac8
CStarn <Christoph.Starnecker@gmail.com>
2020-03-21 22:18:47 +0100

    Add Searchbar in PatientManagement.vue

--- client/src/views/PatientManagement.vue
+++ client/src/views/PatientManagement.vue
@@ -1,33 +1,43 @@
 <template>
-  <v-container>
-    <v-data-table :headers="headers" :items="patients">
-      <template
-              v-slot:item.name="{ item }" >
-        {{ item.firstname + " " + item.lastname }}
-      </template>
-    </v-data-table>
-  </v-container>
+    <v-container>
+        <v-autocomplete label="Search" class="mt-10" :items="patientWithFullname" item-text="fullname" item-value="id"
+                        v-model="selectePatientId" outlined>
+        </v-autocomplete>
+        <v-data-table :headers="headers" :items="patients">
+            <template
+                    v-slot:item.name="{ item }">
+                {{ item.firstname + " " + item.lastname }}
+            </template>
+        </v-data-table>
+    </v-container>
 </template>
 
 <script>
 
-  const headers = Object.freeze([
-    {text: 'Name', value: 'name', align: 'start'},
-    {text: 'Number', value: 'telNumber'},
-  ]);
+    const headers = Object.freeze([
+        {text: 'Name', value: 'name', align: 'start'},
+        {text: 'Number', value: 'telNumber'},
+    ]);
 
-  export default {
-    name: "PatientManagement",
-    data() {
-      return {
-        headers
-      }
-    },
-    computed: {
-      patients() {
-        return this.$store.getters["patients"];
-      }
+    export default {
+        name: "PatientManagement",
+        data() {
+            return {
+                headers,
+                selectePatientId: ""
+            }
+        },
+        computed: {
+            patients() {
+                return this.$store.getters["patients"];
+            },
+            patientWithFullname() {
+                return this.$store.getters["patients"].map(patient => ({
+                    ...patient, "fullname": patient.firstname + " " + patient.lastname
+                }));
+            }
+        }
     }
-  }
+
 </script>
 
--- client/src/views/TodaysPatients.vue
+++ client/src/views/TodaysPatients.vue
@@ -1,10 +1,22 @@
 <template>
-  <h1>Today's Patients</h1>
+  <v-container>
+    <v-row>
+      <v-col class="">
+        <v-card>
+          <v-card-title>Patient 1</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>Patient 1</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
   export default {
-    name: "TodaysPatients"
   }
 </script>
 
