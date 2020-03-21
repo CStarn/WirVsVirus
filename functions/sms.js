dba98ee9d832d185ec8f83963cf9128e38d9274e
CStarn <Christoph.Starnecker@gmail.com>
2020-03-21 16:05:46 +0100

    Refactor files

---
+++ functions/sms.js
@@ -0,0 +1,30 @@
+const functions = require("firebase-functions");
+const url = require('url');
+const from = '+49XXXXXXXXXXX'; //todo insert from telephone number here
+
+const express = require("express");
+const app = express();
+
+/**
+ * Parses the SMS API call.
+ * A call at the URL /sms/postpone/ sends an "appointment postponed" message, and expects the parameters
+ *      - 'to' with the recipient phone number
+ *        (formatted according to E.164: [+][country code][phone number including area code])
+ *      - 'time' with the new instant of the appointment in unix time.
+ * Similarly, a call at /sms/notify/ sends an appointment notification message, and expects only the parameters
+ *      - 'to' with the recipient phone number (see above).
+ *
+ * @type {HttpsFunction}
+ */
+app.get("/", (req, res) => {
+    //we accept postpone and notify as calls
+    if (req.path === "/postpone") {
+
+    } else if (req.path === "/notify") {
+
+    }
+    //extract from, to phone number and message content
+    res.send(req.query);
+});
+
+exports.sms = functions.https.onRequest(app);
