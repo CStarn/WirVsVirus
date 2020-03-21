8e3e142e13041f5c78c1f56b3c6be6227806f46b
CStarn <Christoph.Starnecker@gmail.com>
2020-03-21 17:18:38 +0100

    Add vuex dep

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from "./router"
 import store from "./store"
 import vuetify from './plugins/vuetify';
+import firebase from "firebase/app"
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,20 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    const firebaseConfig = {
+      apiKey: "AIzaSyA-GJwcuAlgGX85lF9wuCoxWZ1WYu8xlLw",
+      authDomain: "wirvsvirus-19373.firebaseapp.com",
+      databaseURL: "https://wirvsvirus-19373.firebaseio.com",
+      projectId: "wirvsvirus-19373",
+      storageBucket: "wirvsvirus-19373.appspot.com",
+      messagingSenderId: "466419990592",
+      appId: "1:466419990592:web:45e558854b370e1f97dfa5",
+      measurementId: "G-KW9PSHTH71"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+  }
 }).$mount('#app')
