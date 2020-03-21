e3b92314bf0d984da72953d8293821086c5d3015
Florian Schmidt <fs.schmidt@tum.de>
2020-03-21 22:43:44 +0100

    implemented notify sms handler

--- functions/sms.js
+++ functions/sms.js
@@ -1,18 +1,23 @@
 const functions = require("firebase-functions");
 const moment = require("moment");
 const express = require("express");
+const authid = functions.config().twilio.authid;
+const token = functions.config().twilio.token;
+const from = "+19095314450";
+const twilio = require("twilio");
+const client = new twilio(authid, token);
 const app = express();
 
 /**
  * Parses the SMS API call.
- * A call at the URL /sms/postpone/ sends an "appointment postponed" message, and expects the parameters
+ * A call at the URL /sms/update/ sends an "appointment updated" message, and expects the parameters
  *      - 'to' with the recipient phone number
  *        (formatted according to E.164: [+][country code][phone number including area code])
  *        <--> without! the +
  *      - 'name' with the name of the patient
  *      - 'time' with the new instant of the appointment in unix time in milliseconds.
  */
-app.get("/postpone", (req, res) => {
+app.get("/update", (req, res) => {
     //we accept postpone and notify as calls
     let to = req.query.to;
     let name = req.query.name;
@@ -29,18 +34,17 @@ app.get("/postpone", (req, res) => {
     //input validation (for later)
 
     //send response for debugging at this point
-    res.send(postponeSMSUnchecked(to, name, time));
+    res.send(updateSMSUnchecked(to, name, time));
 });
 
-
 /**
- * Creates and sends the postpone SMS message without input validation.
+ * Creates and sends the update SMS message without input validation.
  * @param to cell phone number of recipient
  * @param name name of recipient
  * @param time unix timestamp in milliseconds of new appointment
  * @returns {{name: *, to: *, time: *, message: string}}
  */
-function postponeSMSUnchecked(to, name, time) {
+function updateSMSUnchecked(to, name, time) {
     //create moment from unix timestamp and extract clock string and relative time
     const parsedTime = moment(+time);
     const clock = parsedTime.format("HH:mm");
@@ -50,7 +54,7 @@ function postponeSMSUnchecked(to, name, time) {
     to = '+' + to;
 
     //build sms message
-    const message = `${name}, your appointment was postponed. The updated time is: ${clock} (${relative})`;
+    const message = `${name}, your appointment time was updated. The new time is: ${clock} (${relative})`;
 
     //call api
     twilioAPICall(message, to);
@@ -71,39 +75,67 @@ function postponeSMSUnchecked(to, name, time) {
  *      - 'name' with the recipient name.
  */
 app.get("/notify", (req, res) => {
-    res.send(req.query);
+    //we accept postpone and notify as calls
+    let to = req.query.to;
+    let name = req.query.name;
+
+    //handle missing params and return 400
+    if (to === undefined || name === undefined) {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write("Bad request: Expected parameters to and name.");
+        res.end();
+        return;
+    }
+
+    //input validation (for later)
+
+    //send response for debugging at this point
+    res.send(notifySMSUnchecked(to, name));
 });
+
+/**
+ * Creates and sends a notification SMS without input validation.
+ * @param to the recipient phone number
+ * @param name the name of the recipient
+ */
+function notifySMSUnchecked(to, name) {
+    //convert 'to' to string and concatenate the + for the phone number
+    to = '+' + to;
+
+    //build sms message
+    const message = `${name}, your appointment starts now. Please find your way to the practitioner's office.`;
+
+    //call api
+    twilioAPICall(message, to);
+
+    //return json message contents for validation of the data sent to the api at this point
+    return {
+        "to": to,
+        "name": name,
+        "message": message,
+        "executedAt": new Date().toString()
+    };
+}
 
 /**
  * Executes the actual SMS twilio API call.
  * @param message the message to send
- * @param to the clean phone number to send it to
+ * @param to clean phone number to send it to
  */
 function twilioAPICall(message, to) {
     console.log("API call with " + message + ", " + to);
-
-    const authid = functions.config().twilio.authid;
-    const token = functions.config().twilio.token;
-
-    //todo only for verification
-    console.log(authid.substring(0, 5));
-    console.log(token.substring(0, 5));
-
-    const from = "+19095314450";
-
-    const twilio = require("twilio");
-    const client = new twilio(authid, token);
 
     client.messages
         .create({
             body: message,
             from: from,
-            statusCallback: "https://postb.in/b/1584823150027-3751792584080",
             to: to
         })
-        .then(message => console.log(message.sid))
-        .catch(e => {
-            console.log(e);
+        .then(message => {
+            console.log(message.sid);
+        })
+        .catch(err => {
+            throw err;
         });
 }
 
