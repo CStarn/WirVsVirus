6d0847a2d130e134145e2f9ba63f736c7fa3a4d6
Florian Schmidt <fs.schmidt@tum.de>
2020-03-21 15:08:11 +0100

    experimentation with query string path parsing

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,10 @@
 const functions = require("firebase-functions");
+//const querystring = require('querystring');
 
 exports.test = functions.https.onRequest((req, res) => {
     res.send("Test successful!");
+});
+
+exports.send_sms = functions.https.onRequest((req, res) => {
+    res.send(req.path);
 });
