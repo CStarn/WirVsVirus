0292ef92072faf7406e28798a5f6769ba2d4697e
CStarn <Christoph.Starnecker@gmail.com>
2020-03-22 10:24:12 +0100

    Add footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <v-app>
-    <navbar></navbar>
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+    <v-app>
+        <navbar></navbar>
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+        <pageFooter/>
+    </v-app>
 </template>
 
 <script>
-  import Navbar from './components/Navbar';
+    import Navbar from './components/Navbar';
+    import PageFooter from "./components/PageFooter";
 
-  export default {
-    name: 'App',
-    components: {
-      Navbar
-    },
-    created() {
-        this.$store.dispatch("getPatients");
-        this.$store.dispatch("getAppointments");
-    }
-  };
+    export default {
+        name: 'App',
+        components: {
+            PageFooter,
+            Navbar
+        },
+        created() {
+            this.$store.dispatch("getPatients");
+            this.$store.dispatch("getAppointments");
+        }
+    };
 </script>
---
+++ client/src/components/PageFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-footer>
+        <v-container fill-width>
+            <v-row justify="center">
+                <v-col cols="3">
+                    <v-img max-width="100%" max-height="100%" src="@/assets/project_logo.png"></v-img>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-footer>
+</template>
+<script>
+    export default {
+        name: 'pageFooter'
+    }
+</script>
