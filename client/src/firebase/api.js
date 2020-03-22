3bf44fabdb708242cf914129d1a6f7b3685d5ea8
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-03-22 13:01:02 +0100

    fixed param in api method

--- client/src/firebase/api.js
+++ client/src/firebase/api.js
@@ -6,5 +6,5 @@ const TEST_URL = "https://postb.in/1584876792666-3796936711296";
 
 const BASE_URL = process.env.NODE_ENV !== "production" ? TEST_URL : FIREBASE_CLOUD_FUNCTION_URL;
 
-export const sendComeInSMS = (number, name) => axios.post(BASE_URL + "/notify", {params: {number, name}});
+export const sendComeInSMS = (number, name) => axios.post(BASE_URL + "/notify", {params: {to: number, name}});
 
