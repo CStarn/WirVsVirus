dd080769083741816735fc9d0627903c292c69ff
CStarn <Christoph.Starnecker@gmail.com>
2020-03-22 14:27:39 +0100

    Group Patient by Doctor

--- client/src/views/TodaysPatients.vue
+++ client/src/views/TodaysPatients.vue
@@ -1,27 +1,23 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col class="">
-        <v-container>
+      <v-row>
+        <v-col v-for="(appointments, key) in Array.from(groupedAppointments)" :key="key">
           <v-row>
             <v-col>
               <v-card>
                 <v-card-title>
-                  Doktor Miller
+                  {{"Doctor "+ appointments[0]}}
                 </v-card-title>
               </v-card>
             </v-col>
           </v-row>
-          <v-row v-for="appointment in appointments" :key="appointment.id">
-            <v-col>
-              <patient-card :appointment="appointment"/>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-col>
-      <v-col>
-      </v-col>
-    </v-row>
+          <v-row v-for="appointment in appointments[1]" :key="appointment.id">
+          <v-col>
+            <patient-card :appointment="appointment"/>
+          </v-col>
+        </v-row>
+        </v-col>
+      </v-row>
   </v-container>
 </template>
 
@@ -32,11 +28,30 @@
         components: {PatientCard},
         filters: {},
         computed: {
-            appointments() {
-                return this.$store.getters["appointments"];
+            groupedAppointments() {
+                return this.groupBy(this.$store.getters["appointments"], app => app.doctor);
             }
         },
-        methods: {}
+        methods: {
+            groupBy(list, keyGetter) {
+                const map = new Map();
+                list.forEach((item) => {
+                    const key = keyGetter(item);
+                    const collection = map.get(key);
+                    if (!collection) {
+                        map.set(key, [item]);
+                    } else {
+                        collection.push(item);
+                    }
+                });
+                // eslint-disable-next-line no-console
+                console.log(map);
+                // eslint-disable-next-line no-console
+                console.log(Array.from(map));
+
+                return map;
+            }
+        }
     }
 </script>
 
